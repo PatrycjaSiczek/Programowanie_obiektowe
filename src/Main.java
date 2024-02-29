8da5bcbf1c2a057cfb83d047a5bc916af5af42dd
PatrycjaSiczek <patrycjasiczek5@gmail.com>
2024-02-29 17:15:21 +0100

    Zadanie 2

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,11 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Point p1 = new Point(10,10);
+        Point p2 = new Point(100, 100);
+        Segment segment = new Segment(p1, p2);
+        System.out.println(segment.toSvg());
 
-
-
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Point[] points = new Point[10];
     }
 }
